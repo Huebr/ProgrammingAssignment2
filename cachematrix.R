f330306a69ef75cd29f9db969d37621cb4a3e195
HueBr <pedro.jorge@aluno.uece.br>
2021-05-21 17:19:24 -0300

    fixed a typo in code instead of 'x' in line 33 the right is use the 'data' variable

--- cachematrix.R
+++ cachematrix.R
@@ -30,7 +30,7 @@ cacheSolve <- function(x, ...) {
            return(m)
         }
         data <- x$get()
-        m <- solve(x,...)
+        m <- solve(data,...)
         x$setmatrix(m)
         m
 }
